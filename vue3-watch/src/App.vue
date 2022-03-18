2feb865de72359fe052acb10dfa6af4998aa6574
Minhhk <alszn2222@gmail.com>
2022-03-18 21:50:03 +0900

    watch, listrendering

---
+++ vue3-watch/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+	<h1 @click="changeMessage">{{ msg }}</h1>
+	<h1>{{ reversedMessage }}</h1>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			msg: "hello!",
+		};
+	},
+	computed: {
+		reversedMessage() {
+			return this.msg.split("").reverse().join("");
+		},
+	},
+	watch: {
+		// 특정한 데이터들의 변경사항을 감시하는 옵션
+		msg(newValue) {
+			console.log("msg:", newValue);
+		},
+	},
+	methods: {
+		changeMessage() {
+			this.msg = "good!";
+		},
+	},
+};
+</script>
+
+/
+<style></style>
---
+++ vue3-watch/src/클래스-스타일바인딩.vue
@@ -0,0 +1,27 @@
+<template>
+	<h1 :class="{ active: isActive }" @click="activate">
+		Hello?!({{ isActive }})
+	</h1>
+</template>
+
+<script>
+export default {
+	date() {
+		return {
+			isActive: false,
+		};
+	},
+	methods: {
+		activate() {
+			this.isActive = true;
+		},
+	},
+};
+</script>
+
+<style scoped>
+.active {
+	color: red;
+	font-weight: bold;
+}
+</style>
