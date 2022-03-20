3ff99ad1a9779b83c5a2dfa599c3bb9c48c30d73
Minhhk <alszn2222@gmail.com>
2022-03-20 12:18:28 +0900

    component 1

---
+++ vue3-component/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+	<MyBtn>Apple</MyBtn>
+	<p>slot /slot 을 출력할 div 사이에 넣어줘야함</p>
+	<MyBtn :color="color"><span style="color: red">banana</span></MyBtn>
+	<MyBtn large color="blue">ddd</MyBtn>
+	<MyBtn>sss</MyBtn>
+</template>
+
+<script>
+import MyBtn from "~/components/MyBtn";
+
+export default {
+	components: {
+		MyBtn,
+	},
+	data() {
+		return {
+			color: "#000",
+		};
+	},
+};
+</script>
+
+<style></style>
